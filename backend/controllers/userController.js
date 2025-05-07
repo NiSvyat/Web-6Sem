333cce4f196a51733c071a38bbc9c3e698c6c0fa
NiSvyat <n.b163samara@gmail.com>
2025-05-07 18:59:52 +0400

    feat:Web-6Sem Психанул и начал все с нуля 😊😊😊😊😊😊😊😊😊😊😊😊😊😊😊😊😊😊😊😊😊😊😊😊😊😊😊😊😊😊😊😊😊😊😊😊😊😊😊😊😊😊😊😊😊😊😊

---
+++ backend/controllers/userController.js
@@ -0,0 +1,91 @@
+﻿const { User } = require('../models/User');
+const { validateUserData } = require('../middleware/validateData');
+
+
+// Create
+const createUser = async (req, res) => {
+
+  // валидация данных
+  const validation = await validateUserData(req.body);
+  if (!validation.valid) {
+    return res.status(400).json({ message: validation.message });
+  }
+
+  // создание пользователя
+  try {
+    const userData = req.body;
+    const newUser = await User.create(userData);
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(400).json({ error: 'ошибка при создании пользователя', details: error.message });
+  }
+};
+
+// Get
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(400).json({ error: 'ошибка при получении пользователей', details: error.message });
+  }
+};
+
+// Get by ID
+const getUserById = async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    if (!user) {
+      return res.status(404).json({ error: 'пользователь не найден' });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ error: 'ошибка при получении пользователя', details: error.message });
+  }
+};
+
+// Update
+const updateUser = async (req, res) => {
+
+  // валидация данных
+  const userId = req.params.id;
+  const validation = await validateUserData(req.body, userId);
+  if (!validation.valid) {
+    return res.status(400).json({ message: validation.message });
+  }
+
+  // обновление пользователя
+  try {
+    const [updated] = await User.update(req.body, { where: { id: userId } });
+    if (!updated) {
+      return res.status(404).json({ error: 'пользователь не найден' });
+    }
+    const updatedUser = await User.findByPk(userId);
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(400).json({ error: 'ошибка при обновлении пользователя', details: error.message });
+  }
+};
+
+// Delete
+const deleteUser = async (req, res) => {
+  try {
+    const deleted = await User.destroy({
+      where: { id: req.params.id },
+    });
+    if (!deleted) {
+      return res.status(404).json({ error: 'пользователь не найден' });
+    }
+    res.status(204).json();
+  } catch (error) {
+    res.status(400).json({ error: 'ошибка при удалении пользователя', details: error.message });
+  }
+};
+
+module.exports = {
+  createUser,
+  getUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+};
---
+++ backend/models/Event.js
@@ -0,0 +1,65 @@
+﻿const { Model, DataTypes } = require('sequelize');
+const { sequelize } = require('../config/db');
+const User = require('./User');
+
+class Event extends Model {}
+
+// структура модели
+Event.init({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true, // первичный ключ
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: true,
+  },
+  date: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  category: {
+    type: DataTypes.ENUM(
+      'music',
+      'sports',
+      'arts',
+      'business',
+      'technology',
+      'education',
+      'food',
+      'other'
+    ),
+    allowNull: false,
+    defaultValue: 'other'
+  },
+  createdBy: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: User,
+      key: 'id',
+    }, // определение внешнего ключа
+  },
+}, {
+  sequelize,
+  modelName: 'Event',
+  tableName: 'events',
+  timestamps: true, // включение createdAt и updatedAt
+});
+
+// синхронизация модели с базой данных
+const syncModel = async () => {
+  try {
+    await Event.sync({ alter: true }); // Using alter: true to modify existing table
+    console.log('таблица "events" успешно синхронизирована.');
+  } catch (error) {
+    console.error('ошибка при синхронизации таблицы "events":', error);
+  }
+}
+
+module.exports = { Event, syncModel };
